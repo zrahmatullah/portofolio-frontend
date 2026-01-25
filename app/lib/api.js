0ff49edea450ba7f1a2376c3a5a003e294b3ad39
zrahmatullah <azharrahmatullah01@gmail.com>
2026-01-25 18:13:05 +0700

    fetch/api

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,2 +1,4 @@
 // export const API_URL = 'http://127.0.0.1:8000/api'
-export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`
+// export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
